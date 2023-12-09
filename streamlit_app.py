df9ae34476831d83e8a4d503da61c03bbd8895ac
Jemaica_Jhade <152510054+JemaicaJhade@users.noreply.github.com>
2023-12-09 15:10:37 +0800

    Update streamlit_app.py
    Learning how to code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,87 @@
+import requests 
 import streamlit as st
+from streamlit_lottie import st_lottie
 
-st.write('Hello World')
+
+st.set_page_config(page_title="Jemaica_Jhade", page_icon=":heart:", layout="wide")
+
+
+with st.container():
+    st.subheader("Hi, I'm Jemaica Jhade Centino :wave:")
+    st.title("Explore my Computer Engineering Journey through my blog")
+    st.write("Hey there, Welcome to my blog where I'm documenting my journey as a student of computer engineering. This is going to be a rollercoaster ride and a whole lot of caffeinated late nights. The enigma of programming leads me into being more curious as thoughts roaming in my head and wanna make it into reality. ")
+    st.write("To explore and to navigate programming is not easy as typing and making a code, it takes a lot of patient for it to run well and being real through imagination.")
+    st.write("[Message me in gmail >] jemaicajhade.30@gmail.com")
+
+
+with st.container():  
+    st.title("A Gloomy Silhouette :heart:")
+    st.write("I really do love writing a letter and a poem, through pogramming and making a code to create a website I can show to other ink lover and a reader my own poetry. Programming is not easy as what other think of it. Time, effort, and patience is needed to create a program that satisfied the competitive sanity of mine.")
+    
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+             This is a learning program that I create 
+             - This web app is not fully finish. 
+             - This is a sample for fun web app only.
+             - To show my poetry through coding. 
+
+            If this sounds interesting to you, you can visit my github account.    
+            """
+        )
+        st.write("[Github Link Here](https://github.com/JemaicaJhade)")
+                
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("My Projects")
+        st.write("##")
+        st.write(
+            """
+            Learning how to code is fun!
+            I'll show you exactly how to make it.
+            """
+        )    
+        st.markdown("[Watch Video...](https://youtu.be/VqgUkExPvLY?si=rWUN__OdtEXkKfqL)")
+with st.container():       
+    with left_column:
+        st.subheader("Open Letter To Myself")
+        st.write(
+            """
+            I cuddled the pillow looking dejected,
+            lonely night with the loud aching soul
+            feeling drained, feels nothing sense at all.
+            Evening sounds felt solitary and out of things, 
+            doesn't make snap decisions but does overthink. 
+            Brood over things that goes wrong,
+            can't stay in the moment when all my thoughts are running circle for what future will bring.
+
+            Am I gonna make it? What would be my potential condition? Am I gonna feel the bliss that genuine smile of eyes? if not, this feeling well stay melancholic, blues of life with teary eyes? I know I am the only one who can mold my foreseeable future. It always between the inner success and inner failure. The choice is mine if I take a risk and move on to the misc. 
+
+            Constantly in my mind to keep forward with diligence and not be dramatic over things. Am I hypocrite to know what will be the state of being open, if I never do the right ways? if I never choose the path that was bright and happy? cause I always stuck of thinking but I never find myself apply it in reality. I can't let go being in the safe place, my comfort zone, it sucks and it made me sick. I feel locked in isolated mind. Screaming my lungs for relief but found myself dismay and dysphoric.
+            """
+        )
+
+
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("[You can message me in gmail >] jemaicajhade.30@gmail.com")
+
+    
+
+    
+
+
+
+
+    
+            
